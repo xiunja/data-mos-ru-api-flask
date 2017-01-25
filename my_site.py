c2c6b99860b9dd7dd1a978fac895cd94b19db900
Ilya V. Schurov <ilya@schurov.com>
2017-01-25 21:18:40 +0300

    update

--- my_site.py
+++ my_site.py
@@ -1,16 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+app.debug = True
+
+def get_data():
+    url = "http://api.data.mos.ru/v1/datasets/2462/rows"
+    r = requests.get(url)
+    return r.json()
 
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def list_rows():
+    return render_template("list_rows.html",
+                           data=get_data())
 
-
-@app.route('/hello/<name>')
-def hello(name):
-    return "Hello, " + name
+@app.route('/table/<int:n>')
+def show_table(n):
+    data = get_data()
+    row = data[n]
+    return render_template("show_table.html",
+                           table=row)
 
 if __name__ == '__main__':
     app.run()
