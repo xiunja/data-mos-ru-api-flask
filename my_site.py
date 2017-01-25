3cb29620660fd24d0baf95aca87c425d2a789081
Ilya V. Schurov <ilya@schurov.com>
2017-01-25 19:40:48 +0300

    Initial commit

---
+++ my_site.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    return "Hello, " + name
+
+if __name__ == '__main__':
+    app.run()
