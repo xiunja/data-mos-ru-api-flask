aa6e7bd25e46262d5d950ef812e1b49b7f07f113
Ilya V. Schurov <ilya@schurov.com>
2017-02-01 17:47:41 +0300

    gitignore added

--- my_site.py
+++ my_site.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import json
 import requests
 
 app = Flask(__name__)
@@ -21,6 +22,16 @@ def show_table(n):
     row = data[n]
     return render_template("show_table.html",
                            table=row)
+
+@app.route('/json/<int:n>')
+def show_json(n):
+    data = get_data()
+    row = data[n]
+    x, y = zip(*[("{}, {}".format(r['Year'], r['Quarter']), r['Value'])
+                 for r in row['Cells']['IndexValues']])
+
+    return Response(json.dumps({'x': x, 'y': y}),
+                    mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
